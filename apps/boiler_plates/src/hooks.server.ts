717237fa6366eb92bb9c097ecd6a491bba494499
Sungho Yahng <yahngsungho@gmail.com>
2025-01-16 15:18:47 +0900

    paraglide

--- apps/boiler_plates/src/hooks.server.ts
+++ apps/boiler_plates/src/hooks.server.ts
@@ -1,5 +1,7 @@
 import type {Handle} from '@sveltejs/kit'
 import {i18n} from '@repo/base/i18n.ts'
 
-const handleParaglide: Handle = i18n.handle()
+const handleParaglide: Handle = i18n.handle({
+	disableAsyncLocalStorage: true, // Serverless 사용한다고 가정
+})
 export const handle: Handle = handleParaglide
