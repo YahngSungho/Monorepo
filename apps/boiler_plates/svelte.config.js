8a7b2472fcf795b5dbd9e11dc4db72d32f3ed1c6
Sungho Yahng <yahngsungho@gmail.com>
2025-01-23 20:45:10 +0900

    .

--- apps/boiler_plates/svelte.config.js
+++ apps/boiler_plates/svelte.config.js
@@ -1,1 +1,14 @@
-export { default } from '@repo/base/svelte.config.js'
+import adapter from '@sveltejs/adapter-cloudflare'
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
+
+const config = {
+	kit: {
+		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
+		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
+		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
+		adapter: adapter(),
+	},
+	preprocess: [vitePreprocess()],
+}
+
+export default config
