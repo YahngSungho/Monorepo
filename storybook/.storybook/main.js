05ae90626b001faccb63a32002d1b919f5e72717
Sungho Yahng <yahngsungho@gmail.com>
2025-01-24 21:50:37 +0900

    .

--- storybook/.storybook/main.js
+++ storybook/.storybook/main.js
@@ -1,7 +1,7 @@
-import { dirname, join } from "path";
+import { dirname, join } from "path"
 /** @type { import('@storybook/svelte-vite').StorybookConfig } */
 const config = {
-    stories: [
+	stories: [
 		"../../apps/**/*.stories.@(js|ts|svelte)",
 		"../../apps/**/*.mdx",
 		"../../libraries/**/*.stories.@(js|ts|svelte)",
@@ -11,23 +11,23 @@ const config = {
 	],
 
 	addons: [
-				"@storybook/addon-svelte-csf",
-				"@storybook/addon-links",
-        "@storybook/addon-essentials",
-        "@chromatic-com/storybook",
-        "@storybook/addon-interactions",
-        "@storybook/addon-mdx-gfm",
-    ],
+		"@storybook/addon-svelte-csf",
+		getAbsolutePath("@storybook/addon-links"),
+		getAbsolutePath("@storybook/addon-essentials"),
+		getAbsolutePath("@chromatic-com/storybook"),
+		getAbsolutePath("@storybook/addon-interactions"),
+		getAbsolutePath("@storybook/addon-mdx-gfm"),
+	],
 
-    framework: {
-			name: "@storybook/svelte-vite",
-			options: {},
+	framework: {
+		name: getAbsolutePath("@storybook/svelte-vite"),
+		options: {},
 	},
 
-    docs: {},
+	docs: {},
 }
 export default config
 
 function getAbsolutePath(value) {
-    return dirname(require.resolve(join(value, "package.json")));
+	return dirname(require.resolve(join(value, "package.json")))
 }
