9316c37a44e8572d45666bcb93ddb0d0627c37ed
Sungho Yahng <yahngsungho@gmail.com>
2025-02-14 05:59:56 +0900

    feat: tailwindcss 추가 및 환경 변수 정의
    postcss.config.js에서 tailwindcss를 추가했음.  
    .svelte-kit의 ambient.d.ts 파일에 여러 환경 변수를 추가했음.  
    vite.config.js에서 불필요한 tailwindcss2 관련 코드를 제거했음.  
    환경 변수 정의를 통해 애플리케이션의 설정을 명확히 했음.

--- libraries/base/postcss.config.js
+++ libraries/base/postcss.config.js
@@ -1,15 +1,16 @@
 import presetEnv from 'postcss-preset-env'
-// import tailwindcss from '@tailwindcss/postcss'
+import tailwindcss from '@tailwindcss/postcss'
 // Todo: sveltekit에서 postcss.config.js가 의미가 있는 거 맞는지 체크... 혹시 vite.config.js 이거에서 다 설정 되는 거고 이 파일에 적힌 것들은 의미 없는 거 아님?
 const config = {
 	plugins: [
-		// tailwindcss(),
+		tailwindcss(),
 		presetEnv({
 			autoprefixer: { grid: false },
+			// Fix: 따로 파일 만들면 이거 생략가능한지체크
 			browsers: '> 0.2% and not dead',
 			enableClientSidePolyfills: true,
 			minimumVendorImplementations: 2,
-			state: 2,
+			stage: 2,
 		}),
 	],
 }
