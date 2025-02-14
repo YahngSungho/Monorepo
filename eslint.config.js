177a7abbe94ce15627d80d26b354e2d42918429d
Sungho Yahng <yahngsungho@gmail.com>
2025-02-15 02:48:53 +0900

    feat(eslint): import/extensions 규칙 비활성화
    'import/extensions' 규칙을 비활성화했음.  
    TypeScript 프로젝트에서 import 구문에 대한 유연성을 높이기 위함임.

--- eslint.config.js
+++ eslint.config.js
@@ -224,6 +224,7 @@ export default [
 		rules: {
 			'@typescript-eslint/no-explicit-any': 'off',
 			'@typescript-eslint/no-unused-vars': 'off',
+			'import/extensions': 'off',
 			'arrow-parens': 'off',
 			camelcase: 'off',
 			'capitalized-comments': 'off',
