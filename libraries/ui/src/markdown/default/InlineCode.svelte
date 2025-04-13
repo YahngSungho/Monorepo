1d9d70d1994e3a7143fbfec4ff00ed1a589c6e99
Sungho Yahng <yahngsungho@gmail.com>
2025-04-13 21:39:07 +0900

    feat(ui): InlineCode 컴포넌트 스타일 추가
    InlineCode 컴포넌트에 스타일을 추가하여 코드 블록의 시각적 표현을 개선했음. 코드의 배경색과 텍스트 색상을 설정하여 가독성을 높였음.

--- libraries/ui/src/markdown/default/InlineCode.svelte
+++ libraries/ui/src/markdown/default/InlineCode.svelte
@@ -3,3 +3,11 @@ let { children } = $props()
 </script>
 
 <code>{@render children()}</code>
+
+<style>
+	code {
+		border-radius: 0;
+		color: var(--gray-0);
+		background-color: var(--gray-12);
+	}
+</style>
