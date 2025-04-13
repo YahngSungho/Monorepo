3fca9b59bd2d8849f845b4f4a78cef59c715166e
Sungho Yahng <yahngsungho@gmail.com>
2025-04-13 14:18:49 +0900

    feat: 새로운 파일 추가 및 의존성 업데이트
    apps/boiler_plates/src/routes/vanilla.xstate.js 파일 추가함.  
    apps/boiler_plates/src/types/global.d.ts 파일 추가함.  
    libraries/base/src/openPrps-normalize.css 파일 추가함.  
    libraries/ui/src/types/global.d.ts 파일 추가함.  
    pnpm-lock.yaml에서 @csstools/postcss-global-data 및 postcss-custom-media 의존성 업데이트함.  
    package.json에서 @tailwindcss/postcss 및 @tailwindcss/vite 버전 업데이트함.  
    기타 CSS 및 Svelte 파일에서 스타일 및 구조 개선함.  
    
    이 변경사항들은 새로운 기능 추가와 함께 의존성 관리를 통해 프로젝트의 안정성을 높이기 위해 수행됨.

--- libraries/base/postcss.config.js
+++ libraries/base/postcss.config.js
@@ -1,7 +1,24 @@
+import path from 'node:path'
+import { fileURLToPath } from 'node:url'
+
+import postcssGlobalData from '@csstools/postcss-global-data'
 import tailwindcss from '@tailwindcss/postcss'
+import openProps from 'open-props'
+import postcssJitProps from 'postcss-jit-props'
 import presetEnv from 'postcss-preset-env'
+
+// Simulate __dirname in ESM
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
 const config = {
 	plugins: [
+		postcssGlobalData({
+			// presetEnv보다 먼저 실행되도록 추가
+			files: [
+				path.resolve(__dirname, './src/openProps-props-media.css'),
+			],
+		}),
 		tailwindcss(),
 		presetEnv({
 			autoprefixer: { grid: false },
@@ -9,6 +26,13 @@ const config = {
 			minimumVendorImplementations: 2,
 			stage: 3,
 		}),
+		postcssJitProps(openProps),
+    // @ts-ignore
+    postcssJitProps({
+      files: [
+        path.resolve(__dirname, './node_modules/open-props/open-props.min.css'),
+      ]
+    }),
 	],
 }
 
