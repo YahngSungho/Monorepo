1e477c1595c1c6b378cb7013349e2622cac483ad
YahngSungho <29212791+YahngSungho@users.noreply.github.com>
2025-05-14 15:21:10 +0000

    [automated-mutation]

--- libraries/llms/src/gemini/getCache.js
+++ libraries/llms/src/gemini/getCache.js
@@ -3,19 +3,21 @@ import { R } from '@library/helpers/R'
 import { ai } from './ai.js'
 import { latestModel } from './modelNames.js'
 
-export const getCachBySystemInstructions = R.curry(async (modelName, duration, options, willBeCachedData) => {
-	const options0 = options || {}
+export const getCachBySystemInstructions = R.curry(
+	async (modelName, duration, options, willBeCachedData) => {
+		const options0 = options || {}
 
-	const { name: cacheName } = await ai.caches.create({
-		model: modelName,
-		config: {
-			systemInstruction: willBeCachedData,
-			ttl: `${60 * duration}s`,
-			...options0,
-		},
-	})
+		const { name: cacheName } = await ai.caches.create({
+			model: modelName,
+			config: {
+				systemInstruction: willBeCachedData,
+				ttl: `${60 * duration}s`,
+				...options0,
+			},
+		})
 
-	return cacheName
-})
+		return cacheName
+	},
+)
 
 export const getCacheBySystemInstructions_latestModel = getCachBySystemInstructions(latestModel)
