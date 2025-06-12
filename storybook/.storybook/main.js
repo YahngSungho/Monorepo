e215497d76839ee892a188efcfeece65643c9a5d
Sungho Yahng <yahngsungho@gmail.com>
2025-06-12 10:35:35 +0900

    .

--- storybook/.storybook/main.js
+++ storybook/.storybook/main.js
@@ -1,12 +1,12 @@
 import { createRequire } from 'node:module'
 import path from 'node:path'
+
+// @ts-ignore - `instrument` is a valid core feature, but not in the svelte-vite types
 /** @type {import('@storybook/svelte-vite').StorybookConfig} */
 const config = {
 	addons: [
 		'@storybook/addon-svelte-csf',
-		getAbsolutePath('@storybook/addon-links'),
 		getAbsolutePath('@storybook/addon-essentials'),
-		getAbsolutePath('@chromatic-com/storybook'),
 	],
 
 	docs: {},
@@ -17,7 +17,15 @@ const config = {
 	},
 
 	stories: ['../../*/*/src/**/*.stories.@(js|ts|svelte)'],
+
+	features: {
+		// @ts-ignore
+		interactionsDebugger: true, // 디버깅 시 유용
+		// @ts-ignore - 'instrument' is a valid core feature, but not in the svelte-vite types
+		instrument: false, // 이 옵션을 false로 설정
+	},
 }
+
 export default config
 
 function getAbsolutePath(value) {
