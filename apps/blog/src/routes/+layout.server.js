812375479a8318e96f1e8e3358412904b9f4c9e4
Sungho Yahng <yahngsungho@gmail.com>
2025-08-01 22:24:02 +0900

    fix(blog): pinned 아이템 description 생성 방식 수정
    allMetadata 객체를 Object.values로 변환하여 pinned 아이템들의 title을 올바르게 추출하도록 변경했음.  
    이전에는 allMetadata 자체를 배열로 처리하여 description 생성에 오류가 있었음.

--- apps/blog/src/routes/+layout.server.js
+++ apps/blog/src/routes/+layout.server.js
@@ -2,7 +2,7 @@ import { getAllMetadata0 } from '$lib/server/getMetadata'
 
 export const load = async () => {
 	const allMetadata = await getAllMetadata0()
-	const description = allMetadata
+	const description = Object.values(allMetadata)
 	.filter((item) => item.pinned).map((item) => (item.title)).join(' / ')
 
 	return {
