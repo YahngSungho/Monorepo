1d3fe27e3c50b7a24872476bf106ee58e2d90e27
YahngSungho <29212791+YahngSungho@users.noreply.github.com>
2025-09-23 10:36:43 +0000

    [automated-mutation]

--- libraries/helpers/src/markdown/index.js
+++ libraries/helpers/src/markdown/index.js
@@ -13,7 +13,7 @@ export function removeFrontmatter(string) {
 
 export function removeTagsAll(string) {
 	// 입력 문자열에서 모든 태그명 추출 (정규식은 태그명만 가볍게 매칭)
-	const rawMatches = string.matchAll(/<\/?([a-zA-Z][\d:a-zA-Z-]*)\b/gi)
+	const rawMatches = string.matchAll(/<\/?([a-z][\d:a-z-]*)\b/gi)
 	const tagNames = []
 	for (const match of rawMatches) {
 		const startIndex = match.index ?? 0
@@ -26,7 +26,7 @@ export function removeTagsAll(string) {
 			}
 		}
 	}
-	const uniqueTagNames = Array.from(new Set(tagNames))
+	const uniqueTagNames = [...new Set(tagNames)]
 
 	return sanitizeHtml(string, {
 		allowedAttributes: {},
@@ -61,9 +61,9 @@ export function removeMarkdownFormat(string) {
 export function removeMDAndTags(string) {
 	return (
 		removeMarkdownFormat(removeTagsAll(removeFrontmatter(string)))
-		.trim()
-		// eslint-disable-next-line regexp/no-unused-capturing-group
-		.replaceAll(/(\r?\n){2,}/g, '\n')
+			.trim()
+			// eslint-disable-next-line regexp/no-unused-capturing-group
+			.replaceAll(/(\r?\n){2,}/g, '\n')
 	)
 }
 
