7f27e130dc9510650fce4b50bfa15f2140b9bd82
YahngSungho <29212791+YahngSungho@users.noreply.github.com>
2025-09-30 00:33:01 +0000

    [automated-mutation]

--- apps/blog/src/lib/components/postList.svelte
+++ apps/blog/src/lib/components/postList.svelte
@@ -15,33 +15,33 @@ let { allMetadata } = $props()
 	style:inline-size="100%"
 >
 	{#each allMetadata as postMetadata (postMetadata.slug)}
-		<div style="display: flex;">
+		<div style:display="flex">
 			{#snippet link()}
-			<Link
-			class={css`
-				display: block;
-				inline-size: fit-content;
-			`}
-			active={postMetadata.current}
-			href={`/posts/${postMetadata.slug}`}
-		>
-			{postMetadata.title}
-		</Link>
-		{/snippet}
+				<Link
+					class={css`
+						display: block;
+						inline-size: fit-content;
+					`}
+					active={postMetadata.current}
+					href={`/posts/${postMetadata.slug}`}
+				>
+					{postMetadata.title}
+				</Link>
+			{/snippet}
 
-		{#if postMetadata.current}
-		<IconText iconName="mdi:chevron-right">
-			{@render link()}
-		</IconText>
-	{:else if postMetadata.visited}
-		<IconText iconName="mdi:check">
-			{@render link()}
-		</IconText>
-	{:else}
-		<IconText>
-			{@render link()}
-		</IconText>
-	{/if}
+			{#if postMetadata.current}
+				<IconText iconName="mdi:chevron-right">
+					{@render link()}
+				</IconText>
+			{:else if postMetadata.visited}
+				<IconText iconName="mdi:check">
+					{@render link()}
+				</IconText>
+			{:else}
+				<IconText>
+					{@render link()}
+				</IconText>
+			{/if}
 		</div>
 	{/each}
 </div>
