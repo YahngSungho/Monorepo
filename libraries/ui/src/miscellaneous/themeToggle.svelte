1414855572429f14fe19242b3dcfd92a8f6d0c58
YahngSungho <29212791+YahngSungho@users.noreply.github.com>
2025-12-16 12:42:01 +0000

    [automated-mutation]

--- libraries/ui/src/miscellaneous/themeToggle.svelte
+++ libraries/ui/src/miscellaneous/themeToggle.svelte
@@ -7,10 +7,8 @@ let checked = $derived(mode.current === 'dark')
 </script>
 
 <label
-	style="
-border: 1px solid currentcolor;
-font-size: 1rem;
-"
+	style:border="1px solid currentcolor"
+	style:font-size="1rem"
 	class={`toggle text-base-content toggle-${size}`}
 >
 	<input
@@ -24,12 +22,12 @@ font-size: 1rem;
 		type="checkbox"
 	/>
 
-	<span style="inline-size: 28px; block-size: 28px;"
-		><iconify-icon height="20" icon="mdi:white-balance-sunny" width="20"></iconify-icon></span
-	>
-	<span style="inline-size: 28px; block-size: 28px;"
-		><iconify-icon height="20" icon="mdi:weather-night" width="20"></iconify-icon></span
-	>
+	<span style:inline-size="28px" style:block-size="28px">
+		<iconify-icon height="20" icon="mdi:white-balance-sunny" width="20"></iconify-icon>
+	</span>
+	<span style:inline-size="28px" style:block-size="28px">
+		<iconify-icon height="20" icon="mdi:weather-night" width="20"></iconify-icon>
+	</span>
 </label>
 
 <style>
