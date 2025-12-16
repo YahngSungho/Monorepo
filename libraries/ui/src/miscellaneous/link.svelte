2ba3d13a4e8e120d1178cc61815c4f28e1aef184
YahngSungho <29212791+YahngSungho@users.noreply.github.com>
2025-12-16 16:08:16 +0000

    [automated-mutation]

--- libraries/ui/src/miscellaneous/link.svelte
+++ libraries/ui/src/miscellaneous/link.svelte
@@ -30,7 +30,7 @@ let isInternalLink = $derived(href?.startsWith('.') || href?.startsWith('/'))
 	<a
 		class={cx(linkStyle, incomingClass)}
 		href={isInternalLink ? resolve(localizeHref(href), {}) : href}
-		{...((isInternalLink || isHeadingLink) ? {} : newTabProperties)}
+		{...isInternalLink || isHeadingLink ? {} : newTabProperties}
 		{...rest}
 	>
 		{@render children?.()}
