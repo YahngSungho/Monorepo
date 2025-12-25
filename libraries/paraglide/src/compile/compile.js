eefcc374d2ed1ccb399b9f7d4d6764e979f6f654
YahngSungho <29212791+YahngSungho@users.noreply.github.com>
2025-12-25 10:49:43 +0000

    [automated-mutation]

--- libraries/paraglide/src/compile/compile.js
+++ libraries/paraglide/src/compile/compile.js
@@ -2,8 +2,8 @@ import { compile } from '@inlang/paraglide-js'
 
 await compile({
 	disableAsyncLocalStorage: true,
+	experimentalMiddlewareLocaleSplitting: true,
 	outdir: './paraglide-output/',
 	project: './project.inlang/',
 	strategy: ['cookie', 'preferredLanguage', 'url', 'baseLocale'],
-	experimentalMiddlewareLocaleSplitting: true,
 })
