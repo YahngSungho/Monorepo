bf353d2bf205f6c48eb71e4d7f7af0f6bdf933a9
Sungho Yahng <yahngsungho@gmail.com>
2025-12-31 15:20:45 +0900

    feat(blog): locale 기반 markdown 로딩 지원 추가
    - getLocale 인자를 통해 locale 전달하도록 getOneMarkdown/getOneMarkdownBody/getAllMetadataObject 인터페이스 변경
    - routes에서 getLocale 사용하여 locale 기반으로 post 및 metadata 로드하도록 수정
    - 불필요한 request 파라미터 제거 및 호출 시 인자 전달 방식 정리
    
    BREAKING CHANGE: getOneMarkdown, getOneMarkdownBody, getAllMetadataObject 시그니처가 변경되어 호출부 수정 필요함.

--- apps/blog/src/routes/+layout.server.js
+++ apps/blog/src/routes/+layout.server.js
@@ -1,7 +1,8 @@
 import { getAllMetadataObject } from '$lib/wrappers/markdown/getMetadata'
+import { getLocale } from '@library/paraglide/helpers'
 
-export const load = async ({ request }) => {
-	const allMetadataObject = await getAllMetadataObject()
+export const load = async () => {
+	const allMetadataObject = await getAllMetadataObject(getLocale())
 	const description = Object.values(allMetadataObject)
 		.filter((item) => item.pinned)
 		.map((item) => item.title)
